e004af095fc1a6e2b7d388fb5d010a9c31486394
paulcodeman <paulcodeman@users.noreply.github.com>
2026-07-17 23:27:53 +0200

    fix: BackTextBlock use-after-free + error messages + output options

---
+++ apps/eolite/test_with_kfont.c
@@ -0,0 +1,9 @@
+#define MEMSIZE 1024 * 250
+#include "../lib/clipboard.h"
+#include "../lib/strings.h"
+#include "../lib/mem.h"
+#include "../lib/fs.h"
+#include "../lib/gui.h"
+#include "../lib/list_box.h"
+#include "../lib/random.h"
+#include "../lib/kfont.h"
